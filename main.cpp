323f149b90b50f7f2f86322ee64ada2ead97ff0c
alho caralho <dann.ru@proton.me>
2025-07-02 17:00:42 +0100

    whatever this is, doesnt work

--- main.cpp
+++ main.cpp
@@ -9,30 +9,55 @@ int main() {
 
     class player {
     public:
-       int size = (40,40);
-       Vector2 pos = {0,0};
+       int size;
+       Vector2 pos;
     };
 
     player P1;
     P1.pos = Vector2{450,490};
-    P1.size = 40;
+    P1.size = 20;
 
     InitWindow(screenWidth, screenHeight, "70708");
 
     SetTargetFPS(60); 
 
-    if (IsKeyDown(KEY_LEFT)) {
-        P1.pos += Vector2Add(P1.pos, {-10,0});
-    }
-
     while (!WindowShouldClose()) {
+
+        if (IsKeyDown(KEY_LEFT)) {
+            P1.pos += Vector2{-160,0} / 60;
+        }
+
+        if (IsKeyDown(KEY_RIGHT)) {
+            P1.pos += Vector2{160,0} / 60;
+        }
+
+        if (IsKeyDown(KEY_UP)) {
+            P1.pos += Vector2{0,-160} / 60;
+        }
+
+        if (IsKeyDown(KEY_DOWN)) {
+            P1.pos += Vector2{0,160} / 60;
+        }
+
+        if (P1.pos.x >= screenWidth/2 - 140 + 280 - P1.size) {
+            P1.pos.x = screenWidth/2 - 140 + 280 - 1;
+        } else if (P1.pos.x <= screenWidth/2 - 140 - P1.size) {
+            P1.pos.x = screenWidth/2 - 140;
+        }
+
+        if (P1.pos.y >= screenHeight/2 + 10 + 280 - P1.size) {
+            P1.pos.y = screenHeight/2 + 10 + 280;
+        } else if (P1.pos.y <= screenHeight/2 + 10 - P1.size) {
+            P1.pos.y = P1.pos.y - 1;
+        }
+
         DrawFPS(0,0);
 
         BeginDrawing();
             ClearBackground(BLACK);
-            DrawRectangle(screenWidth/2 - 150, screenHeight/2, 300, 300, BLUE);
+            DrawRectangle(screenWidth/2 - 150, screenHeight/2, 300, 300, RED);
             DrawRectangle(screenWidth/2 - 140, screenHeight/2 + 10, 280, 280, BLACK);
-            DrawRectangle(P1.pos.x, P1.pos.y, P1.size, P1.size, ORANGE);
+            DrawCircle(P1.pos.x, P1.pos.y, P1.size, WHITE);
         EndDrawing();
     }
     return 0;
